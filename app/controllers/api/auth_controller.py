6262c0dc1b662b8fe37f0ddcac522f8facdf3867
Bibek Lamichhane <lamichhane.kebib@gmail.com>
2025-08-20 10:40:46 +0545

    controllers

---
+++ app/controllers/api/auth_controller.py
@@ -0,0 +1,35 @@
+from models import dto
+from service import user_service
+from core.security import session
+from core import dependencies
+from fastapi import APIRouter, status, Response
+
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["Auth"]
+)
+
+@router.post("/register", status_code=status.HTTP_201_CREATED, response_model=dto.UserDTO)
+async def register(user: dto.UserCreateDTO):
+    return user_service.create_user(user)
+
+@router.post("/login", status_code=status.HTTP_200_OK, response_model=str)
+async def login(obj: dto.UserLoginDTO, res: Response):
+    return await session.login(obj, res)
+
+@router.get("/logout", status_code=status.HTTP_204_NO_CONTENT)
+async def logout(res: Response):
+    await session.logout(res)
+
+@router.get("/validate", response_model=dto.Token)
+async def check_session(token: dependencies.token_dependency):
+    return token
+
+@router.put("/password/update", status_code=204)
+def update_password(dto: dto.UserUpdatePasswordDTO, user: dependencies.user_dependency):
+    user_service.update_password(user, dto)
+
+@router.post("/password/reset", status_code=204)
+def reset_password(email: str):
+    user_service.reset_password(email)
