c201bd5b790dfc57db267965f70718f0bb6d1184
Bibek Lamichhane <lamichhane.kebib@gmail.com>
2025-09-12 12:21:14 +0545

    **Add user verification and update user repo**
    - Add `get_by_verification_token` to `user_repo.py` for fetching users by email verification token
    - Update `update` and `delete` methods in `user_repo.py` to use `UUID` instead of `int`
    - Update `user_service.py` to include user verification email functionality
    - Add email validation and error handling in `create_user` method
    - Import necessary modules and update function signatures to use `UUID`

--- app/repos/user_repo.py
+++ app/repos/user_repo.py
@@ -1,3 +1,4 @@
+from uuid import UUID
 from app.models.db import UserDB
 from app.core.db_context import session_maker
 
@@ -7,17 +8,31 @@ def add(user: UserDB)-> UserDB:
         session.add(user)
         return user
 
-def update(user: UserDB) -> None:
-    with session_maker.begin() as session:
-        session.query(UserDB).filter(UserDB.id == user.id).update({
-            UserDB.name: user.name,
-            UserDB.surname: user.surname,
-            UserDB.role: user.role,
-            UserDB.email: user.email,
-            UserDB.password: user.password
-        })
 
-def delete(id: int) -> None:
+def get_by_verification_token(token: str) -> UserDB:
+    """Get user by email verification token"""
+    with session_maker() as session:
+        return session.query(UserDB).filter(
+            UserDB.email_verification_token == token
+        ).first()
+
+def update(user: UserDB) -> UserDB:
+    """Update user in database"""
+    with session_maker.begin() as session:
+        session.merge(user)
+        return user
+# def update(user: UserDB) -> None:
+#     with session_maker.begin() as session:
+#         session.query(UserDB).filter(UserDB.id == user.id).update({
+#             UserDB.name: user.name,
+#             UserDB.role: user.role,
+#             UserDB.email: user.email,
+#             UserDB.password: user.password,
+#             UserDB.is_active: user.is_active,
+#             UserDB.email_verification_token: user.email_verification_token
+#         })
+
+def delete(id: UUID) -> None:
     with session_maker.begin() as session:
         session.query(UserDB).filter(UserDB.id == id).delete()
 
@@ -25,7 +40,7 @@ def get(limit:int = 1000, offset: int = 0) -> list[UserDB]:
     with session_maker() as session:
         return session.query(UserDB).limit(limit).offset(offset).all()
 
-def get_by_id(id: int) -> UserDB | None:
+def get_by_id(id: UUID) -> UserDB | None:
     with session_maker() as session:
         return session.query(UserDB).where(
             UserDB.id == id
