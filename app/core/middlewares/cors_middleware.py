737ae5d9436bf60d7d53abd3e640e7f967d05091
Bibek Lamichhane <lamichhane.kebib@gmail.com>
2026-03-19 15:50:55 +0545

    added rate limit

--- app/core/middlewares/cors_middleware.py
+++ app/core/middlewares/cors_middleware.py
@@ -1,11 +1,11 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
-
+from app.core.config import CONFIG
 
 def add(app: FastAPI):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=[CONFIG.FRONTEND_URL],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
